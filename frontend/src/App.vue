746ed9dd58fa2a2d7685126d5d5d06e6f55dc045
Thiago Bartels <tfbartels@gmail.com>
2018-07-27 16:48:51 -0300

    Commit inicial Frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>Header</el-header>
+      <el-container>
+        <el-aside width="200px">Aside</el-aside>
+        <el-container>
+          <el-main>
+            <router-view/>
+          </el-main>
+        </el-container>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+  .el-header {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+
+  .el-main {
+    text-align: center;
+  }
+
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+</style>
---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,86 @@
+<template>
+ <div>
+    <el-card class="box-card" shadow="hover">
+      <div slot="header" class="clearfix">
+        <span>Card name</span>
+        <el-button style="float: right; padding: 3px 0" type="text">Operation button</el-button>
+      </div>
+      <div v-for="o in 4" :key="o" class="text item">
+        {{'List item ' + o }}
+      </div>
+    </el-card>
+    <el-table
+      :data="tableData"
+      height="1000"
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        align="left"
+        label="Date"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        align="left"
+        label="Name"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        align="left"
+        prop="address"
+        label="Address">
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      tableData: [{
+        date: '2016-05-03',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-02',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-04',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }, {
+        date: '2016-05-01',
+        name: 'Tom',
+        address: 'No. 189, Grove St, Los Angeles'
+      }]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .text {
+    font-size: 14px;
+  }
+
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+
+  .box-card {
+    width: 480px;
+  }
+</style>
